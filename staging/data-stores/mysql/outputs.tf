60b937e1852c4d1f72e6a7e12857a9c92ac22494
Koshban <kaushik.banerjee77@gmail.com>
2022-12-31 17:45:06 +0800

    fixing stuff

--- staging/data-stores/mysql/outputs.tf
+++ staging/data-stores/mysql/outputs.tf
@@ -1,4 +1,9 @@
 output "address" {
     value = aws_db_instance.example.address
-  
+    description = "DB connection endpoint"  
+}
+
+output "port" {
+    value = aws_db_instance.example.port
+    description = "The port the database is listening on"  
 }
--- staging/services/WebServer/provider.tf
+++ staging/services/WebServer/provider.tf
@@ -1,6 +1,6 @@
 # Provider
 provider "aws" {
     region      = "ap-southeast-1"
-    access_key  = "AKIAWC5B2DHPPL5PKB2I"
-    secret_key  = "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"
+    # access_key  = "   "
+    # secret_key  = "  "
 }
