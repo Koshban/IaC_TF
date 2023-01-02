89691f8788550de2ccf5e9d8fdf0d028e543aa35
Koshban <kaushik.banerjee77@gmail.com>
2023-01-02 23:54:31 +0800

    fixing typo in concat

--- live/Global/iAM/output.tf
+++ live/Global/iAM/output.tf
@@ -11,6 +11,6 @@ output "all_arn" {
 # The one function takes a list as input and if the list has 0 elements, it returns null; if the list has 1 element, it returns that element; 
 # and if the list has more than 1 element, it shows an error. Putting them together we can find which access returned values and then print it
 output "neo_cloudwatch_policy_arn" {
-    value = concat(one(aws_iam_user_policy_attachment.neo_cloudwatch_full_access[*].policy_arn,
+    value = one(concat(aws_iam_user_policy_attachment.neo_cloudwatch_full_access[*].policy_arn,
                        aws_iam_user_policy_attachment.neo_cloudwatch_read_only[*].policy_arn))  
 }
