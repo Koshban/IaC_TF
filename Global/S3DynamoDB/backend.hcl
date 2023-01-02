05bee0e903c095731db8943bff569c865d4a76dd
Koshban <kaushik.banerjee77@gmail.com>
2023-01-02 13:16:58 +0800

    gitignore changes

--- Global/S3DynamoDB/backend.hcl
+++
@@ -1,5 +0,0 @@
-bucket          = "kaushikb-terraform-s3-state"
-region          = "ap-southeast-1"
-dynamodb_table  = "kaushikb-terraform-s3-state"
-encrypt         = true
-profile         = "credentials"
--- Global/S3DynamoDB/backend.tf
+++
@@ -1,9 +0,0 @@
-# Backend to link storing TF State in S3 and DynamoDB
-terraform {
-  backend "s3" {
-    # bucket = "kaushikb-terraform-s3-state"  # Your S3 bucket name
-    key     = "global/s3/terraform.tfstate"  # The filepath within the S3 bucket where the Terraform state file should be written
-    encrypt = true
-    profile = "credentials"
-  }
-}
