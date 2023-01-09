0f640f4cf4932c3042d2e29a799cd7b3e04efaf2
Koshban <kaushik.banerjee77@gmail.com>
2023-01-09 21:21:36 +0800

    fixing parent account

--- variousthings/multiaccount/provider.tf
+++ variousthings/multiaccount/provider.tf
@@ -25,10 +25,10 @@ provider "aws" {
     }
 }
 
-# data "aws_caller_identity" "parent" {
-#     provider = aws.parent
-# }
+data "aws_caller_identity" "parent" {
+    provider = aws.parent
+}
 
-# data "aws_caller_identity" "child" {
-#     provider = aws.child
-# }
+data "aws_caller_identity" "child" {
+    provider = aws.child
+}
